9359a236bc955a84b53417246fb5b4b2e3d04389
tuvistavie <tuvistavie@gmail.com>
2013-03-10 03:20:00 +0900

    Add test for nonexisting file extensions.

--- i18n/tests/loader_tests.py
+++ i18n/tests/loader_tests.py
@@ -1,10 +1,11 @@
 import unittest
 
-import i18n
+from i18n import resource_loader
 
 class TestFileLoader(unittest.TestCase):
-    def test_dummy(self):
-        self.assertTrue(hasattr(i18n, 'resource_loader'))
+    def test_nonexisting_extension(self):
+        self.assertRaises(resource_loader.I18nFileLoadError, resource_loader.load_resource, "foo.bar")
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestFileLoader)
 unittest.TextTestRunner(verbosity=2).run(suite)
