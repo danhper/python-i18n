6bb539ae56b9172a4efa6e2e2c557556de402ce0
tuvistavie <tuvistavie@gmail.com>
2013-03-16 01:42:02 +0900

    Add basic tests for translation.

--- i18n/tests/translation_tests.py
+++ i18n/tests/translation_tests.py
@@ -4,6 +4,7 @@ import os.path
 
 from i18n import resource_loader
 from i18n.translator import t
+from i18n import translations
 from i18n import config
 
 RESOURCE_FOLDER = os.path.dirname(__file__) + os.sep + 'resources' + os.sep
@@ -12,9 +13,19 @@ class TestTranslationFormat(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         resource_loader.init_loaders()
-        config.get('load_path').append(os.path.join(RESOURCE_FOLDER, 'translations'))
+        config.set('load_path', [os.path.join(RESOURCE_FOLDER, 'translations')])
+        translations.add('foo.hi', 'Hello %{name} !')
 
     def test_basic_translation(self):
         self.assertEqual(t('foo.normal_key'), 'normal_value')
-        config.set('file_format', 'json')
-        self.assertEqual(t('bar.baz.qux'), 'hoge')
+
+    def test_basic_placeholder(self):
+        self.assertEqual(t('foo.hi', name='Bob'), 'Hello Bob !')
+
+    def test_missing_placehoder(self):
+        self.assertEqual(t('foo.hi'), 'Hello %{name} !')
+
+    def test_missing_placeholder_error(self):
+        config.set('error_on_missing', True)
+        with self.assertRaises(KeyError):
+            t('foo.hi')
