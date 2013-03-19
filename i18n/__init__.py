01224122a20e65de75d64a6d2a94a51230afd1c5
tuvistavie <tuvistavie@gmail.com>
2013-03-19 16:06:07 +0900

    Add imports in __init__ file.

--- i18n/__init__.py
+++ i18n/__init__.py
@@ -1,1 +1,9 @@
+from . import resource_loader
 from .resource_loader import I18nFileLoadError, register_loader
+from .translator import t
+from .translations import add as add_translation
+from . import config
+
+resource_loader.init_loaders()
+
+load_path = config.get('load_path')
