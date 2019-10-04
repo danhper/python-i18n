996efcc7acbbb900ef0009d454a2d3be247832da
Daniel Perez <daniel@perez.sh>
2019-10-04 12:05:06 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-i18n',
-    version='0.3.6',
+    version='0.3.7',
     description='Translation library for Python',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
