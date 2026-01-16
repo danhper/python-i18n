6708771c1df392101a73910ffc81e493b4af9780
xinvxueyuan <xinvxueyuan@yeah.net>
2026-01-16 23:58:53 +0800

    refactor: Improve import statements for clarity

--- i18n/__init__.py
+++ i18n/__init__.py
@@ -1,14 +1,11 @@
 from . import config, resource_loader
-from .config import get as get, set as set
-from .resource_loader import (
-    I18nFileLoadError as I18nFileLoadError,
-    load_config as load_config,
-    register_loader as register_loader,
-)
+from .config import get as get
+from .config import set as set
+from .resource_loader import I18nFileLoadError as I18nFileLoadError
+from .resource_loader import load_config as load_config
+from .resource_loader import register_loader as register_loader
 from .translations import add as add
 from .translator import t as t
 
-
 resource_loader.init_loaders()
-
 load_path = config.get("load_path")
